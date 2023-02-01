51a7a184186a4c89934ede698d3140ac81925e6d
Seth Barlow <sethdbarlow@gmail.com>
2023-02-01 15:24:42 -0700

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left - 1}px`;
+}
+     
+
+dodger.addEventListener('keydown', (e) => {
+    if(e.key === 'ArrowLeft'){
+        moveDodgerLeft();
+    }
+})
+
+function moveDodgerRight(){
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+    if(right < 360){
+    dodger.style.left = `${right +10}px`;
+    }
+
+
+}
+
+dodger.addEventListener('keydown', (e) => {
+    if(e.key === 'ArrowReft'){
+        moveDodgerRight();
+    }
+})
